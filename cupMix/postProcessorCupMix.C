2508ea67e74ca5cde89cd9d7fa23138ae456c28f
srebughini <ste.rebu@gmail.com>
2014-12-21 14:52:56 +0100

    Add cupMix

---
+++ cupMix/postProcessorCupMix.C
@@ -0,0 +1,65 @@
+// OpenSMOKE++ Definitions
+#include "OpenSMOKEpp"
+
+// CHEMKIN maps
+#include "maps/Maps_CHEMKIN"
+
+// Reactor utilities
+#include "reactors/utilities/Utilities"
+
+// Additional classes
+#include "catalyticReactorClass.H"
+
+// ODE solvers
+#include "math/multivalue-ode-solvers/MultiValueSolver"
+#include "ode/ODE_Parameters.h"
+
+// OpenFOAM
+#include "fvCFD.H"
+#include "multivariateScheme.H"
+#include "pimpleControl.H"
+#include "fvIOoptionList.H"
+
+// Additional include files
+#include "sparkModel.H"
+#include "userDefinedFunctions.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+	#include "setRootCase.H"
+	#include "createTime.H"
+	#include "createMesh.H"
+	#include "readGravitationalAcceleration.H"
+
+	#include "createBasicFields.H"
+	#include "readSolverOptions.H"
+	#include "createAdditionalFields.H"
+	#include "createCatalyticFields.H"
+	#include "createFvOptions.H"
+	#include "catalystTopology.H"
+	#include "memoryAllocation.H"
+	#include "properties.H"
+	#include "finalSetupOperations.H"
+
+	#include "initContinuityErrs.H"
+	#include "readTimeControls.H"
+	#include "compressibleCourantNo.H"
+	#include "setInitialDeltaT.H"
+
+	pimpleControl pimple(mesh);
+
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+	while (runTime.run())
+	{
+		runTime++;
+		runTime.write();
+		Info<< "Run only one FAKE time step!!!" << endl;
+	}
+
+    return 0;
+}
+
+// ************************************************************************* //
