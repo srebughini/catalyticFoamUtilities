8060a98f43517e9dbdbd132e63501c4f4c797ab6
srebughini <ste.rebu@gmail.com>
2015-07-08 11:59:40 +0200

    Improve fields evaluation, now it is working for all OpenFOAM

--- cupMixForRhoSimpleFoam/cupMix.C
+++ cupMixForRhoSimpleFoam/cupMix.C
@@ -16,22 +16,21 @@ int main(int argc, char *argv[])
     #include "cupMixFields.H"
     #include "createFvOptions.H"
     #include "initContinuityErrs.H"
-    #include "cupMixFieldsEvaluation.H"
 
     Info<< "\nStarting FAKE time loop\n" << endl;
 
-	bool cupMixExit = true;
-	while (runTime.run())
-	{
-		if (cupMixExit != true)
-		{
-			Info << "ERROR: please set writeNow in controlDict options" << endl;
-			exit(-1);
-		}
-		runTime++;
-		runTime.write();
-		cupMixExit = false;
-	}
+    bool cupMixExit = true;
+    while (runTime.run())
+    {
+        if (cupMixExit != true)
+        {
+            Info << "ERROR: please set writeNow in controlDict options" << endl;
+            exit(-1);
+        }
+        runTime++;
+        runTime.write();
+        cupMixExit = false;
+    }
 
     return 0;
 }
