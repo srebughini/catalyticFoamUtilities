a948581639819ced266332a0a02ca699807ebbdf
Stefano Rebughini <ste.rebu@gmail.com>
2016-09-01 15:50:41 +0200

    Improved cup mixing post processing

---
+++ cupMixForCatalyticMultiRegionSimpleFoam/solver/catalyticMultiRegionSimpleFoamCupMix.C
@@ -0,0 +1,65 @@
+// OpenSMOKE++ Definitions
+#include "OpenSMOKEpp"
+
+//C++
+#include <functional>
+#include <algorithm>
+
+// CHEMKIN maps
+#include "maps/Maps_CHEMKIN"
+
+// Reactor utilities
+#include "reactors/utilities/Utilities"
+
+// OpenFOAM
+#include "fvCFD.H"
+#include "multivariateScheme.H"
+#include "regionProperties.H"
+
+// Additional include files
+#include "userDefinedFunctions.H"
+#include "reactions/reactionRates.H"
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+
+    regionProperties rp(runTime); 
+
+    #include "createFluidMeshes.H" 
+    #include "createSolidMeshes.H" 
+
+    #include "readSolverOptions.H"
+    #include "readFluidProperties.H"
+    #include "readSolidProperties.H"
+
+    #include "createFluidFields.H"
+    #include "createSolidFields.H"
+
+    #include "propertiesFluid.H"
+    #include "propertiesSolid.H"
+
+    #include "createFinalFields.H"
+
+    #include "initContinuityErrs.H"
+    
+    #include "cupMix.H"
+
+	bool cupMixExit = true;
+	while (runTime.loop())
+	{
+		if (cupMixExit != true)
+		{
+			Info << "ERROR: please set writeNow in controlDict options" << endl;
+			exit(-1);
+		}
+		runTime++;
+		runTime.write();
+		cupMixExit = false;
+	}
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
